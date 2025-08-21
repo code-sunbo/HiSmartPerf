556f4297f4f598626f08b7d22423f7f8fabd267b
code-sunbo <bb.sunbo@huawei.com>
2025-08-21 20:22:58 +0800

    update

--- smartperf_host/trace_streamer/build.sh
+++ smartperf_host/trace_streamer/build.sh
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 set -e
+echo "$OSTYPE"
 PARAMS=$*
 SOURCE="${BASH_SOURCE[0]}"
 cd "$(dirname "${SOURCE}")"
